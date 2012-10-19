c09b51a4aa8669d8d98c4d5fa0d61cdb11c18a13
Adrien Jarthon <jobs@adrienjarthon.com>
2012-10-19 21:51:07 +0200

    initial rails app

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+# Use unicorn as the app server
+gem 'unicorn'
+
+# Deploy with Capistrano
+gem 'capistrano'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+E2tvWebapp::Application.config.secret_token = 'bcc8876f2384c481f102a26f1d8571a4d97ad71fa04b55fe0319c7659a41d7391d7acbef68f7fc88f34abaf6e91a682832e2ffef48480bbf34064a400482a410'

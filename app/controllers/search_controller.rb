bb70772f6ad2b165df68f2ec158c96e52bfefaaa
Adrien Jarthon <jobs@adrienjarthon.com>
2012-10-20 20:41:30 +0200

    search#show

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,17 @@ class SearchController < ApplicationController
   end
 
   def show
-    @details = JSON.parse(`../e2tv-api/e2tv --details --#{params[:type]} #{params[:id]}`)
+    @details = Rails.cache.fetch("search#show:details:#{params[:type]}:#{params[:id]}") do
+      JSON.parse(`../e2tv-api/e2tv --details --#{params[:type]} #{params[:id]}`)
+    end
+    @sources = Rails.cache.fetch("search#show:sources:#{params[:type]}:#{params[:id]}") do
+      JSON.parse(`../e2tv-api/e2tv --sources --#{params[:type]} #{params[:id]}`)
+    end
+    @image = (@details['/common/topic/image'].first || {})['id']
+    @trailer = @sources.select {|s| s['kind'] == 'Trailer'}.first
+    @rental = @sources.select {|s| s['kind'] == 'Rental'}.first
+    @streaming = @sources.select {|s| s['kind'] == 'Streaming'}.first
+    @torrents = @sources.select {|s| s['kind'] == 'Torrent'}
   end
 
 end

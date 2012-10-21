929b26b7769c4fc9153a96a02f6e23643231688b
Adrien Jarthon <jobs@adrienjarthon.com>
2012-10-21 10:29:19 +0200

    scrol

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -3,7 +3,6 @@ class ProgramsController < ApplicationController
   def index
     @torrents = Torrent::get_all
     @schedule = schedule['channels']
-    puts @schedule.first.inspect
     @start = @schedule.map {|c| Time.parse(c['programs'].first['from']) }.sort.first
   end
 
@@ -23,7 +22,6 @@ class ProgramsController < ApplicationController
 private
 
   def schedule
-  #  Rails.cache.delete("schedule")
     Rails.cache.fetch("schedule") do
       JSON.parse(`./script/pgep.py`)
     end
